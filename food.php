a13cdddba1fab9f45dc1e1793e375a8622bf4a72
Tedwinder <TedCallow76@gmail.com>
2016-02-04 15:33:43 -0800

    Update food.php

--- food.php
+++ food.php
@@ -2,40 +2,23 @@
 //define('THIS_PAGE', basename($_SEVER['PHP_SELF']))
 //food truck.php
 
-
-
-
 class Item{
     public $name = ''; 
     public $price = 0;
     public $description='';
     
-public function __construct($name,$description,$price){
+public function Item($name,$description,$price){
     
     $this->name = $name;
     $this->description = $description;   
     $this->price = $price;
         
- 
-    }
-    
-  
+    }  
 }
-
-
-
 
 define('THIS_PAGE', basename($_SERVER['PHP_SELF']));
 
-    
- //echo THIS_PAGE;
-//die;
-    
-    
-    //check for input from the user
-    
-    ?>
-
+?>
 
 <html>
 <head>
@@ -48,69 +31,65 @@ define('THIS_PAGE', basename($_SERVER['PHP_SELF']));
     
     <label><h1>Fried Rice</h1></label>
     <p>mixed with other ingredients, such as eggs, vegetables, and meat!</p>
-    <input type="text" name="input1" value=""> Quantity<br>  
+    <input type="text" name="input1" value="0"> Quantity<br>  
     
     <!--food item 2-->
     
     <label><h1>Phat Thai</h1></label>
     <p>stir-fried rice noodle dish!</p>
-    <input type="text" name="input2" value=""> Quantity<br> 
+    <input type="text" name="input2" value="0"> Quantity<br> 
     
-      <!--food item 3-->
-    
+      <!--food item 3-->    
     
      <label><h1>Pizza</h1></label>
     <p>topped with a selection of meats, vegetables and condiments</p>
     
-    <input type="text" name="input3" value=""> Quantity<br> 
+    <input type="text" name="input3" value="0"> Quantity<br> 
     
     <!--submit button-->
     
-    <input type="submit" name="submit" value="submit">
-    
-    
-
-    
+    <input type="submit" name="submit" value="submit">    
 </form>   
 </html>
     
 <?PHP
-    if(isset($_POST['submit']))
-
-{
+    if(isset($_POST['submit'])){
     
 //post the values from the form to local variables
     
     $item1Quantity= $_POST['input1'];
     $item2Quantity= $_POST['input2'];
-    $item3Quantity= $_POST['input3'];
-        
+    $item3Quantity= $_POST['input3'];        
 
 //create an array using the constructor class
+
+if ($item1Quantity >=0 && is_numeric($item1Quantity)) {
+
+        $item[] = new Item("Fried rice","mixed with other ingredients, such as eggs, vegetables, and meat!", 8.99);
+        echo 'you have ordered ' . $item1Quantity . ' of ' . $item[0]->$name;
+    
+    }else if($item2Quantity >=0 && is_numeric($item2Quantity)) {
         
-/*if ('$item1Quantity'>0){
+        $item[] = new Item("Phat Thai","stir-fried rice noodle dish!" ,3.99);
+        echo 'you have ordered ' . $item2Quantity . ' of ' . $item[1]->$name;
     
+    }else if($item3Quantity >=0 && is_numeric($item3Quantity)) {
     
-$item1[] = new Item("Fried rice","mixed with other ingredients, such as eggs, vegetables, and meat!" ,8.99);
-echo "you've ordered" . $item1Quantity . "of" $item1[1];
+        $item[] = new Item("Pizza","topped with a selection of meats, vegetables and condiments" ,2.99);
+        echo 'you have ordered ' . $item3Quantity . ' of ' . $item[2]->$name;
     
-    }else if isset('$item2Quantity') {  
-        
-$item2[] = new Item("phat Thai","stir-fried rice noodle dish!" ,3.99);
+    }else {
     
-    }else if isset('$item3Quantity')
-    
-$item3[] = new Item("pizza","topped with a selection of meats, vegetables and condiments" ,2.99);  
+        echo 'something has gone wrong';
+        die;
     }
 
-  print_r($item3);  */    
         
 //calculate the subtotal
     
     $subTotal=($item1Quantity*8.99) + ($item2Quantity*3.99) + ($item3Quantity*2.99);
         
-//calculate the tax
-    
+//calculate the tax    
     
     $tax = number_format(($subTotal * 0.095),2);
         
@@ -123,11 +102,7 @@ $item3[] = new Item("pizza","topped with a selection of meats, vegetables and co
     echo"<table><tr><th>Tax: </th></tr>
         <tr><th>$tax</th></tr><br>";
     echo"<table><tr><th>Total: </th></tr>
-        <tr><th>$total</th></tr><br>";
-    
-     
-    
+        <tr><th>$total</th></tr><br>";    
 }
     
 ?>
-
